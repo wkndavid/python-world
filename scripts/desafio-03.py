4b5a2bf1ff0fc231fc124a9f534272f3d79c1daf
wkndavid <workingnown@gmail.com>
2023-01-04 14:53:42 -0300

    Desafios das primeiras aulas do segundo módulo

--- scripts/desafio-03.py
+++ scripts/desafio-03.py
@@ -1,4 +1,6 @@
 num1 = int (input('Digite o Primeiro número:'))
 num2 = int (input('Digite o Segundo número:'))
 res = num1 + num2
-print(res)
+# print('A soma vale entre '+str(num1)+' e '+str(num2)+ ', vale: ', res)
+
+print('A soma entre {} e {} vale: {}'.format(num1, num2, res))
---
+++ scripts/segundo-modulo/deafio-01.py
@@ -0,0 +1,3 @@
+msg = input('Digite algo: ')
+print(msg.isnumeric())
+print(type(msg))
