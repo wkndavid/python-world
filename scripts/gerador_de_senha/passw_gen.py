f0b092af1a209d51fcc74373414f5e026ce34407
wkndavid <workingnown@gmail.com>
2023-01-16 20:21:51 -0300

    Gerador com modulo random

---
+++ scripts/gerador_de_senha/passw_gen.py
@@ -0,0 +1,13 @@
+import random
+import PySimpleGUI as sg
+import os
+
+class PassGen:
+    def __init__(self):
+        pass
+    def Iniciar(self):
+        pass
+    def salvar_senha(self):
+        pass
+gen = PassGen()
+gen.Iniciar()
---
+++ scripts/segundo-modulo/desafio-somando.py
@@ -0,0 +1,4 @@
+n1 = int (input('Digite um numero: '))
+n2 = int(input('Digite um numero: '))
+res = n1 + n2
+print('O resutado da soma entre {} e {} é {}!'.format(n1, n2, res))
