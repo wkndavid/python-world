e8aaa8472768ff73a8269667f8ef05240e052d2a
wkndavid <workingnown@gmail.com>
2023-06-25 16:12:38 -0300

    Exercício antecessor e sucessor

---
+++ scripts/segundo-modulo/desafio-antecessor-sucessor.py
@@ -0,0 +1,5 @@
+####################################
+# Antecessor e Sucessor
+n = int(input('Digite um número: '))
+
+print('Analisando o valor {}, seu antecessor é {} e seu sucessor é {}'.format(n, (n-1), (n+1)))
