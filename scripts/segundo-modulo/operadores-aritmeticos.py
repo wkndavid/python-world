65720caf09f9bfb0517bebb5fca6b18aee0ba865
wkndavid <workingnown@gmail.com>
2023-06-25 00:55:57 -0300

    Operadores Aritiméticos, exercício de soma

---
+++ scripts/segundo-modulo/operadores-aritmeticos.py
@@ -0,0 +1,17 @@
+# Operadores Aritméticos
+
+###################################################
+
+# Adição: +
+# Subtração: -
+# Multiplicação: *
+# Divisão: /
+# Potência: **
+# Divisão Inteira: //
+# Resto da dicisão: %
+
+n = int(input('Digite um número de 0 a 10: '))
+n1 = int(input('Digite outro: '))
+res = (n + n1)
+
+print('A soma entre {} e {} é: {}!'.format(n, n1, res))
