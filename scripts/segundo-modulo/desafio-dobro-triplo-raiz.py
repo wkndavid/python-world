6bc8386a00b4edeab65ab81c31534bd3ab95d460
wkndavid <workingnown@gmail.com>
2023-06-25 16:45:57 -0300

    Exercício de multiplicação e raiz quadrada

---
+++ scripts/segundo-modulo/desafio-dobro-triplo-raiz.py
@@ -0,0 +1,6 @@
+#############################################
+#   Exercício de dobro/triplo&raizQuadrada  #
+n = int(input('Digite um número: '))
+
+print('O dobro de {} é: {}'.format(n, (n*2)))
+print('O triplo é {} e sua raiz quadrada {:.2f}!'.format((n*3), pow(n, (1/2))))
