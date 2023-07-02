15e285c63d0a5e67282bcd15a8903295b6a337c5
wkndavid <workingnown@gmail.com>
2023-07-01 23:45:44 -0300

    Reajuste Salarial

---
+++ scripts/segundo-modulo/reajuste-salarial.py
@@ -0,0 +1,3 @@
+salario = float(input('Digite o valor do salário: '))
+novo = salario + (salario * 15 / 100)
+print('o salário é de {}R$, e com o reajuste de aumento em 15% ficou {}R$'.format(salario, novo))
