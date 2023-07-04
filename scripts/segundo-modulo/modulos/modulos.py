2b2cd8f39604879c30ea64982be86e3318f3829f
wkndavid <workingnown@gmail.com>
2023-07-04 10:14:05 -0300

    Usando métodos de um módulo

--- scripts/segundo-modulo/modulos/modulos.py
+++ scripts/segundo-modulo/modulos/modulos.py
@@ -2,4 +2,4 @@ import math
 from math import sqrt, floor
 num = int(input('Digite um número: '))
 raiz = sqrt(num)
-print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+print('A raiz de {} é igual a {}'.format(num, floor(raiz)))
