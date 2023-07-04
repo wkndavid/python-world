938e1b64daa6a3710d602a660e3f3bbc48dce1fe
wkndavid <workingnown@gmail.com>
2023-07-03 23:40:11 -0300

    Conversor de Temperatura em graus Celsius e Fahrenheit

---
+++ scripts/segundo-modulo/celsius-fahrenheit.py
@@ -0,0 +1,3 @@
+celsius = float(input('Informe uma temperatura em °C : '))
+fahrenheit = 9 * celsius / 5 + 32
+print('A temperatura de {}°C Celsius em graus Farenheit é {}°F'.format(celsius, fahrenheit))
