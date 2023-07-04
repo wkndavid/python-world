d65c3a3aec47866108bf281cabcd7ffa284cec60
wkndavid <workingnown@gmail.com>
2023-07-04 00:00:43 -0300

    Segunda Parte

--- scripts/segundo-modulo/aluguel-de-carros.py
+++ scripts/segundo-modulo/aluguel-de-carros.py
@@ -1,4 +1,4 @@
 dias = int(input('Quantos dias alugados: '))
 km = float(input('Quantos km rodados: '))
-pago = dias * 60
-print('O total a pagar é de R${:2f}'.format(pago))
+pago = (dias * 60) + (km * 0.15)
+print('O total a pagar é de R$ {}'.format(pago))
