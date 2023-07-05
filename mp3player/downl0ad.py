416dc9b09dfd3b3ffbba0f7d73443a42ee864d95
wkndavid <workingnown@gmail.com>
2023-07-05 09:26:50 -0300

    Salvando arquivo

--- mp3player/downl0ad.py
+++ mp3player/downl0ad.py
@@ -3,3 +3,8 @@ import os
 yt = YouTube(str('URL do vídeo: '))
 ###### Extraindo áudio ######
 video = yt.streams.filter(only.audio=tTrue).first()
+###### Salvando no diretório ######
+print('Qual destino? (/var/www/python-world/mp3player/download)')
+destination = str(input(">> "))
+###### Saída ######
+saida = video.download(output_path=destination)
