87b98156681fe4553e47fa02a9db650f990a251d
wkndavid <workingnown@gmail.com>
2023-07-06 00:15:32 -0300

    Adicionando método input() para poder tocar

--- mp3player/playmp3.py
+++ mp3player/playmp3.py
@@ -1,5 +1,6 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load('Vintage Culture, Rooftime - I Will Find - Zuffo Remix.mp3')
+pygame.mixer.music.load("Vintage Culture, Rooftime - I Will Find - Zuffo Remix.mp3")
 pygame.mixer.music.play()
+input()
 pygame.event.wait()
